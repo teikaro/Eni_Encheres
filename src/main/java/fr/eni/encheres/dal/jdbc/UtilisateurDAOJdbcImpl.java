c3c0af5faa663d49d10d5fb6d217b4d659945a60
saleem51 <saleem51@hotmail.fr>
2022-09-22 10:02:56 +0200

    UtilisateurDAOJdbcImpl (avec selectById)

--- src/main/java/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
+++ src/main/java/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
@@ -12,6 +12,7 @@ import fr.eni.encheres.dal.ConnectionProvider;
 public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
 	public static final String selectByMail = "SELECT no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur FROM UTILISATEURS WHERE email = ?";
 	public static final String selectByPseudo = "SELECT * FROM UTILISATEURS WHERE pseudo = ?";
+	public static final String selectById = "SELECT * FROM UTILISATEURS WHERE no_utilisateur = ?";
 
 	public Utilisateur getUserByLogin(String login, String requete) {
 		Utilisateur user = null;
@@ -42,4 +43,41 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
 	public Utilisateur getUserByPseudo(String login) {
 		return getUserByLogin(login, selectByPseudo);
 	}
+	
+	
+	
+
+
+	public Utilisateur getUserById(Integer id, String requete) {
+		Utilisateur user = null;
+		try (Connection cnx = ConnectionProvider.getConnection();
+				PreparedStatement pstmt = cnx.prepareStatement(requete)){
+					pstmt.setInt(1, id);
+					try(ResultSet rs = pstmt.executeQuery()){
+						if(rs.next()) {
+							user = new Utilisateur(rs.getInt("no_utilisateur"), rs.getString("pseudo"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), 
+									rs.getString("telephone"), rs.getString("rue"), rs.getString("code_postal"), rs.getString("ville"), 
+									rs.getString("mot_de_passe"), rs.getInt("credit"), rs.getBoolean("administrateur"));
+						}
+					}
+				} catch (SQLException e) {
+
+					e.printStackTrace();
+				}
+		return user;
+	}
+
+	@Override
+	public Utilisateur getUserById(Integer id) {
+		return getUserById(id, selectById);
+	}
+
+	@Override
+	public void inserUtilisateur(Utilisateur user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
 }
