4421f12b1a5128702b08e4a516927692881a9eff
Teikaro <48406803+teikaro@users.noreply.github.com>
2022-09-26 23:06:45 +0200

    Modification DAO factory

--- src/main/java/fr/eni/encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/encheres/dal/DAOFactory.java
@@ -1,11 +1,16 @@
 package fr.eni.encheres.dal;
 
+import fr.eni.encheres.dal.jdbc.ArticleDAOJdbcImpl;
 import fr.eni.encheres.dal.jdbc.UtilisateurDAOJdbcImpl;
 
 public abstract class DAOFactory {
 
 	public static UtilisateurDAO getUtilisateurDAO() {
 		return new UtilisateurDAOJdbcImpl();
+	}
+
+	public static ArticleDAO getArticleDAO() {
+		return new ArticleDAOJdbcImpl();
 	}
 	
 	
